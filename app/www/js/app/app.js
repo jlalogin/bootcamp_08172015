c211b86082172fbc8b5399b846ed42e28d8164f5
Eric W. Greene <eric@training4developers.com>
2015-09-03 08:04:02 -0700

    added more widget demos

--- app/www/js/app/app.js
+++ app/www/js/app/app.js
@@ -1,8 +1,6 @@
 (function() {
 
-	"use strict";
-
-	//var widget;
+	//"use strict";
 
 	var dependencies = [
 		//"jquery", "underscore", "backbone", "marionette", "app/app.templates.hbs",
@@ -27,23 +25,78 @@
 			return node;
 		}
 
-		declare("UserFileRow", [Evented, _WidgetBase, _TemplatedMixin], {
+		var UserFileRow = declare("UserFileRow", [Evented, _WidgetBase, _TemplatedMixin], {
 
 			templateString: [
 				"<tr>",
 				"<td data-dojo-attach-point='fileNameCol'></td>",
 				"<td data-dojo-attach-point='fileDescCol'></td>",
-				"<td data-dojo-attach-point='actionCol'>",
-				"<a href='#' data-dojo-attach-event='click: editRow'>Edit</a>",
-				"</td>",
+				"<td data-dojo-attach-point='actionCol'></td>",
 				"</tr>"
 			].join(""),
 
-			fileName: "Test File",
-			_setFileNameAttr: { node: "fileNameCol", type:"innerText" },
+			fileName: "",
+			//_setFileNameAttr: { node: "fileNameCol", type:"innerText" },
 
-			fileDesc: "Test Desc",
-			_setFileDescAttr: { node: "fileDescCol", type:"innerText" },
+			fileDesc: "",
+			//_setFileDescAttr: { node: "fileDescCol", type:"innerText" },
+
+			// Life Cycle Events
+
+			// Step 1: Constructor
+			// Initialize variables and such...
+			constructor: function () {
+				console.log("constructor");
+				console.log(this.fileName);  // blank
+	    },
+
+			// Step 2: Post Mixin Properties
+			// Called just before render, last chance to do anything before DOM elements are created
+			// Note: no need to call base, its an empty function
+	    postMixInProperties: function () {
+				console.log("post mix in prop");
+				console.log(this.fileName); // populated
+	    },
+
+			// Step 3: Build Rendering
+			// Renders the widget
+			buildRendering: function() {
+				console.log("build rendering");
+
+				// sample code of what base does
+				// this.domNode = domConstruct.toDom(this._stringRepl(this.templateString), this.ownerDocument);
+
+				// must call base for templating to work
+				this.inherited(arguments);
+			},
+
+			// Step 4: Post Create
+			// After rendering, but before the widget is attached to the DOM
+			// Note: no need to call base, its an empty function
+			XpostCreate: function() {
+				console.log("post create");
+				this.viewRow();
+			},
+
+			// a call to placeAt occurs here
+
+			// Step 5: Startup
+			// Widget is now live, must be called manually
+			startup: function() {
+				console.log("startup");
+
+				// must call base method for events to work
+				this.inherited(arguments);
+			},
+
+			// Step 6: Destroy
+			// Clean up the widget (no need to overri)
+			destroy: function() {
+				console.log("destroy");
+
+				// must call base to get default destroy behavior
+				this.inherited(arguments);
+			},
 
 			editRow: function() {
 
@@ -110,14 +163,22 @@
 
 		ready(function() {
 
-			parser.parse().then(function() {
-
-				userFileRow.on("update", function(e) {
-					console.log("user file row update");
-					console.dir(e.data);
-				});
-
+			var ufr = new UserFileRow({
+				fileName: "Test File 5",
+				fileDesc: "Test Desc 5"
 			});
+
+			ufr.placeAt(query("tbody")[0]);
+			ufr.startup();
+
+			ufr.on("update", function(e) {
+				console.log("user file row update");
+				console.dir(e.data);
+			});
+
+			setTimeout(function() {
+				ufr.destroy();
+			},10000);
 
 		});
 
